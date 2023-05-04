e76ed55c74869429ca82f8f15fc4f0b4363b05e2
dzigr <dmbrag@gmail.com>
2023-05-04 19:12:09 +0300

    Add 15th step

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name='articles'),
     path('<int:id>/', views.ArticleView.as_view(), name='article_id'),
+    path('create/', views.ArticleFormCreateView.as_view(), name='article_create')
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Article
+from .forms import ArticleForm
 
 
 class IndexPageView(View):
@@ -19,3 +20,17 @@ class ArticleView(View):
         return render(request, 'articles/show.html', context={
             'article': article,
         })
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request, 'articles/create.html', {'form': form})
