f6eb79eff20ae09a9d040eb6c9e31dbcbd5e7e19
dzigr <dmbrag@gmail.com>
2023-05-06 18:43:54 +0300

    Update with 16th lesson

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -3,6 +3,7 @@ from django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name='articles'),
+    path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='article_edit'),
     path('<int:id>/', views.ArticleView.as_view(), name='article_id'),
-    path('create/', views.ArticleFormCreateView.as_view(), name='article_create')
+    path('create/', views.ArticleFormCreateView.as_view(), name='article_create'),
 ]
