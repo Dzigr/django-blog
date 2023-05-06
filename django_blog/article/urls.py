6cf6d4f4e3827c18139ac09fab8e20ad234565b1
dzigr <dmbrag@gmail.com>
2023-05-06 19:38:55 +0300

    Update with delete CRUD

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -4,6 +4,7 @@ from django_blog.article import views
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name='articles'),
     path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='article_edit'),
+    path('<int:id>/delete/', views.ArticleFormDeleteView.as_view(), name='article_delete'),
     path('<int:id>/', views.ArticleView.as_view(), name='article_id'),
     path('create/', views.ArticleFormCreateView.as_view(), name='article_create'),
 ]
